46dacc08f32d21565accad076d043faf019dfc30
codervibe <3217277135@qq.com>
2024-04-26 13:06:09 +0800

    Initial commit

---
+++ IPAddressQuery.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -*- 作者： codervibe -*-
+# -*- 时间: 18 ：46 -*-
+# -*- 获取某个城市的天气 -*-
+# -*-  1.9  -*-
+
+
+import requests
+import argparse
+import json
+
+
+def get_parameter():
+    parser = argparse.ArgumentParser(description='查看IP的归属地')
+    parser.add_argument('-a', dest='ipaddr', type=str, default='', help='输入查询IP')
+    # 设定一个参数 c 或者 city 将这个参数传入并查询
+    parser.add_argument('-v', help='Optional parameters', action='version', version="IPAddressQuery version 1.2.0")
+    args = parser.parse_args()
+    if not args.ipaddr:
+        parser.print_help()
+        exit()
+    ipaddr = args.ipaddr
+
+    return ipaddr
+
+
+def get_json(ipaddr):
+    url = 'http://ip-api.com/json/{}?lang=zh-CN'.format(ipaddr)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+        'Connection': 'keep-alive'
+    }
+
+    r = requests.get(url, timeout=15, headers=headers)
+    r.close()
+    result = r.content.decode()
+    return result
+
+
+def main():
+    ipaddr = get_parameter()
+    ip_str = get_json(ipaddr)
+    ip_json = json.loads(ip_str)
+    # print(ip_json)
+    # 国家
+    ip_country = ip_json['country']
+    # 城市
+    ip_city = ip_json['city']
+    # IP地址
+    ip_query = ip_json['query']
+    # 地区名称
+    ip_regionName = ip_json['regionName']
+    # 时区
+    ip_timezone = ip_json['timezone']
+    #  lat是纬度的意思。lon经度，经线的意思。
+    # 经度
+    ip_lon = ip_json['lon']
+    # 纬度
+    ip_lat = ip_json['lat']
+    # 互联网服务提供商
+    ip_isp = ip_json['isp']
+    print('查询的IP：{}\n归属地为:{},{},{}\n时区:{}\t'.format(ip_query, ip_country,
+                                                    ip_regionName, ip_city, ip_timezone))
+
+    print('经度:{}\t纬度:{}\t'.format(ip_lon, ip_lat))
+
+    print('互联网服务提供商:\t{}'.format(ip_isp))
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,13 @@
+#  IPAddressQuery
+~~~shell
+usage: IPAddressQuery [-h] [-a IPADDR] [-v]
+
+查看IP的归属地
+
+options:
+  -h, --help  show this help message and exit
+  -a IPADDR   输入查询IP
+  -v          Optional parameters
+
+~~~
+* 支持 proxychains4 代理 查询
